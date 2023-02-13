7af58f6df66cb2a597b3cdc4be18f23d8c09743d
Enzo Gomes <110132900+enzogomezz@users.noreply.github.com>
2023-02-13 17:54:28 -0300

    Update script.js
    alterações feitas pra finalizar a tabuada com condições de falha

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function biloto() {
-    let numero = Number(document.querySelector("input#numeral").value)
+    let numero = document.querySelector("input#numeral")
+    let tabela = document.querySelector("select#res")
     let conta = 1
-    let res = document.querySelector("div#res")
-    res.innerHTML = ""
-    do {
-        let opres = numero*conta
-        console.log(`${conta} x ${numero} = ${opres}`)
-        res.innerHTML += `${conta} x ${numero} = ${opres} </br>`
-        conta++
-    } while (conta <= 10)
+
+    if (numero.value.length == 0) {
+        window.alert("[ERRO] Impossivel calcular")
+    } else {
+        tabela.innerHTML = ""
+        do {
+            let criaItem = document.createElement("option")
+            criaItem.text = `${Number((numero).value)} x ${conta} = ${Number((numero).value) * conta}`
+            criaItem.value = `tab${conta}`
+            tabela.appendChild(criaItem)
+            conta++
+        } while (conta <= 10)
+    }
 }
